fc1d2cb148dd8e3ed92de204d6d0154bc30a82f3
Ramasubramanya-MS <81099796+Ramasubramanya-MS@users.noreply.github.com>
2022-04-19 23:21:59 +0530

    Update SortingVisualizer.jsx

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -19,7 +19,7 @@ export default class SortingVisualizer extends React.Component {
 
   resetArray() {
     const array = [];
-    for (let i = 0; i < 180; i++) {
+    for (let i = 0; i < 190; i++) {
       array.push(randomIntFromInterval(5, 730));
     }
     this.setState({array});
